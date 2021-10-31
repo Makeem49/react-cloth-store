7efb280ab65c8a1f646dd43660a9a703189d2f29
Moshood Akeem Olayinka <moshoodakeemolayinka@gmail.com>
2021-10-31 21:09:38 +0100

    Adding cart component folder

---
+++ src/Cart component/Cart.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function Cart() {
+    return <>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta molestias ipsam natus possimus excepturi fuga praesentium consectetur repellendus qui neque.</p>
+    </>
+}
