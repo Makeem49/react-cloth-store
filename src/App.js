bc6233182c13b0bd06a9c6bd6a35b0e4880041dc
Moshood Akeem Olayinka <moshoodakeemolayinka@gmail.com>
2021-10-31 20:53:16 +0100

    Adding route and links to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from "react";
 import Navbar from "./Navbar Component/Navbar";
 import Container from "./Container component/Container";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import About from "./About component/About";
+import Cart from "./Cart component/Cart";
+import ProductDetails from "./ProdcutDetails/ProductDetails";
 
 export default function App() {
   return <>
-        <BrowserRouter>
-          <Navbar></Navbar>
-          <Container />
-        </BrowserRouter>
+    <BrowserRouter>
+        <Navbar></Navbar>
+            <Switch>
+                <Route exact path='/'>
+                    <Container />
+                </Route>
+                <Route exact path='/about'>
+                    <About />
+                </Route>
+                <Route exact path='/login'>
+                    <h1>Login container here</h1>
+                </Route>
+                <Route exact path='/cart'>
+                    <Cart />
+                </Route>
+                <Route exact path='/products/:id'>
+                    <ProductDetails />
+                </Route>
+        </Switch>
+    </BrowserRouter>
   </>
 }
