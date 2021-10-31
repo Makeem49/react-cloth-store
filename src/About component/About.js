551cdb918c1d434a136b2c2154813641c503283b
Moshood Akeem Olayinka <moshoodakeemolayinka@gmail.com>
2021-10-31 21:09:09 +0100

    Adding about component folder

---
+++ src/About component/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function About() {
+    return (
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, doloremque! Officia sed at possimus, accusantium ipsam officiis excepturi! Nemo sed tempore nostrum numquam asperiores quae consequuntur voluptas, provident ipsa placeat!
+            </p>
+        </div>
+    )
+}
