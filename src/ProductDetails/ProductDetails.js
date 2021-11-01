9ac25c6a493b1b4bddcada7abda3f57d4dcf6fc7
Moshood Akeem Olayinka <moshoodakeemolayinka@gmail.com>
2021-11-01 09:09:33 +0100

    Adding description and reviews to product

--- src/ProductDetails/ProductDetails.js
+++ src/ProductDetails/ProductDetails.js
@@ -4,11 +4,22 @@ import { useRouteMatch, useParams } from "react-router";
 import Loader from "../Loader";
 import './ProductDetails.css'
 
-export default function ProductDetails() {
+export default function ProductDetails(props) {
+    // console.log(props)
     const {get} = useFetch('https://fakestoreapi.com')
     const {url} = useRouteMatch()
     const [isLoading, setIsLoading] = useState(true)
     const [product, setProduct] = useState({})
+    const {quantity, toHandleDecrease, toHandleIncrease} = props
+
+    const amount = product.price * 0.2
+    const discount = product.price - amount
+    const amountOff = product.price * 0.8
+    const discountPer = product.price - amountOff
+
+    const randomNum = (min,max) => {
+        return Math.floor(Math.random() * (max - min) + max )
+    }
 
     useEffect(() => {
         get('/'+url)
@@ -37,10 +48,51 @@ export default function ProductDetails() {
                         {product.description}
                     </p>
                 </div>
-                <h2>Price :NGN  {product.price * 1000}</h2>
-                <h4>Discount :  </h4> <span>20% off</span>
+                <h2>Price :NGN {discount.toFixed(2) * 100}</h2>
+                <h4>Discount :NGN { discountPer.toFixed(2) * 100 }  </h4> <span>20% off</span>
+                <div className="category">
+                    <p> Category :  {product.category}</p>
+                </div>
+                <div className="rating">
+                    <p><strong>rating</strong> : {product?.rating.rate}</p>
+                </div>
+
+
+                {/* <div className="cart__details--quantity">
+                        <div className="quantity">
+                            <p className='quantity__q'>Quantity : {quantity} </p>
+                            <button onClick={toHandleDecrease} disabled={quantity === 0}>-</button>
+                            <button onClick={toHandleIncrease}>+</button>
+                        </div>
+                </div>
+                <button className='cart'>ADD TO CART</button> */}
+
+
+
+
            </div>
        </div>
+
+
+        <div className="reviews">
+            <h2>Product reviews : </h2>
+
+            <div className="user">
+                <h3>John Kyle </h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel at minus aperiam blanditiis officia non eveniet error facilis obcaecati pariatur.</p>
+            </div>
+
+            <div className="user">
+                <h3>Kyle Loris</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel at minus aperiam blanditiis officia non eveniet error facilis obcaecati pariatur.</p>
+            </div>
+
+            <div className="user">
+                <h3>Doe Kevin.</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel at minus aperiam blanditiis officia non eveniet error facilis obcaecati pariatur.</p>
+            </div>
+        </div>
+
     </div>
         
         
