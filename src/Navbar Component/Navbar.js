ec78b149e1e4f8bb26e78918b905f2ad0b1fc2c7
Moshood Akeem Olayinka <moshoodakeemolayinka@gmail.com>
2021-11-08 20:21:09 +0100

    Making navbar responsive

--- src/Navbar Component/Navbar.js
+++ src/Navbar Component/Navbar.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useContext, useState} from "react";
 import { Link } from "react-router-dom";
 import './Navbar.css'
+import '../index.css'
+import 'font-awesome/css/font-awesome.min.css';
 import './../logo.svg'
+import { ThemeContext } from "../helpers/ThemeProvider";
+import clsx from "clsx";
 
 
 export default function Navbar() {
-    return <nav>
-        <div className="nav">
+    const mode = useContext(ThemeContext)
+    const {theme, toggleTheme} = mode
+    const [sidebar, setSiderbar] = useState(false)
+    const className = clsx({show : sidebar, sidebar : true})
+    let themeStatus = null;
+
+
+    if (theme === 'dark') {
+        themeStatus = 'light'
+    } else {
+        themeStatus = 'dark'
+    }
+    
+
+    return <>
+    <nav>
+        <div className={`nav`}>
             <div className="col logo">
                 <Link to='/'>
                     <img src={'logo.svg'} alt="" />
@@ -19,8 +38,37 @@ export default function Navbar() {
                     <li className='links_li--row'><Link to='/about'>About</Link></li>
                     <li className='links_li--row'><Link to='/login'>Login</Link></li>
                     <li className='links_li--row'><Link to='/cart'>Cart</Link></li>
+                    <li className='links_li--row'><button className={`mode`} onClick={() => toggleTheme()}> {themeStatus}</button></li>
                 </ul>
+            </div>
+
+
+            <div className="hamburger">
+                <i className="fa fa-bars" onClick={() => setSiderbar(!sidebar)}></i>
             </div>
         </div>
     </nav>
+
+    <div className={className}>
+                <ul className='sidebar__li'>
+                    <li className='sidebar_li--row' onClick={() => setSiderbar(!sidebar)} ><Link to='/'>Home</Link></li>
+                    <li className='sidebar_li--row' onClick={() => setSiderbar(!sidebar)}><Link to='/about'>About</Link></li>
+                    <li className='sidebar_li--row' onClick={() => setSiderbar(!sidebar)}><Link to='/login'>Login</Link></li>
+                    <li className='sidebar_li--row' onClick={() => setSiderbar(!sidebar)}><Link to='/cart'>Cart</Link></li>
+
+                    <li className='sidebar_li--row' onClick={() => setSiderbar(!sidebar)}>
+                        <button className='mode' onClick={() => toggleTheme()}> 
+                            {themeStatus}
+                        </button>
+                    </li>
+                </ul>
+
+                <div className="sidebar_bottom">
+                    <p>Footer sidebar</p>
+                </div>
+    </div>
+
+    </>
+
+    
 }
