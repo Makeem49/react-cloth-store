6e8d70b3b5a2073c9d3ee99106b5641665053981
Moshood Akeem Olayinka <moshoodakeemolayinka@gmail.com>
2021-11-08 20:17:27 +0100

    Wrapping components with Mainwrapper to access context value

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
-import React from "react";
+import React, {useContext} from "react";
 import Navbar from "./Navbar Component/Navbar";
 import Container from "./Container component/Container";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import About from "./About component/About";
 import Cart from "./Cart component/Cart";
 import ProductDetails from "./ProductDetails/ProductDetails.js";
+import ThemeProvider from "./helpers/ThemeProvider";
+import './index.css'
+import { ThemeContext } from "./helpers/ThemeProvider";
+
+
+
+
+function MainWrapper() {
+    const mode = useContext(ThemeContext)
+    const {theme} = mode
+    return <div className={`${theme}`}>
+        <Navbar></Navbar>
+                <Switch>
+                    <Route exact path='/'>
+                        <Container />
+                    </Route>
+                    <Route exact path='/about'>
+                        <About />
+                    </Route>
+                    <Route exact path='/login'>
+                        <h1>Login container here</h1>
+                    </Route>
+                    <Route exact path='/cart'>
+                        <Cart />
+                    </Route>
+                    <Route exact path='/products/:id'>
+                        <ProductDetails />
+                    </Route>
+            </Switch>
+    </div>
+}
 
 export default function App() {
-  return <>
-    <BrowserRouter>
-        <Navbar></Navbar>
-            <Switch>
-                <Route exact path='/'>
-                    <Container />
-                </Route>
-                <Route exact path='/about'>
-                    <About />
-                </Route>
-                <Route exact path='/login'>
-                    <h1>Login container here</h1>
-                </Route>
-                <Route exact path='/cart'>
-                    <Cart />
-                </Route>
-                <Route exact path='/products/:id'>
-                    <ProductDetails />
-                </Route>
-        </Switch>
-    </BrowserRouter>
-  </>
+  return <ThemeProvider>
+    <>
+        <BrowserRouter>
+            <MainWrapper />
+        </BrowserRouter>
+    </>
+  </ThemeProvider>
 }
